1917437aa56157223010fbc35c5531af0e43e94a
PeterFiorentino <peterfiorentino@pursuit.org>
2020-03-08 10:56:56 -0400

    adding a transaction page to the database and changing the API call to intraday and occur every 5 minutes

--- backend/database.sql
+++ backend/database.sql
@@ -11,7 +11,16 @@ CREATE TABLE users (
     cash INT
 );
 
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users (id),
+    quantity INT NOT NULL,
+    ticker VARCHAR NOT NULL
+)
+
 INSERT INTO users (username, email, password, cash)
     VALUES ('PeteFiorentino', 'PFiorentino54@gmail.com','Spotify123', 5000);
 
 SELECT * FROM users WHERE email = 'PFiorentino54@gmail.com';
+
+INSERT INTO 
--- frontend/src/Components/Portfolio.jsx
+++ frontend/src/Components/Portfolio.jsx
@@ -11,9 +11,9 @@ class Portfolio extends React.Component {
 
     handlePurchase = async (e) => {
         e.preventDefault();
-        let price = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.state.ticker}&interval=60min&apikey=4IVCYEP8YDVPEZ23`)
+        let price = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${this.state.ticker}&interval=5min&apikey=4IVCYEP8YDVPEZ23`)
         let today = price.data[ 'Meta Data' ][ '3. Last Refreshed' ]
-        let todaysPrice = price.data[ 'Time Series (Daily)' ][today]
+        let todaysPrice = price.data[ 'Time Series (5min)' ][today]
         console.log(price)
         console.log(today)
         console.log(todaysPrice)
