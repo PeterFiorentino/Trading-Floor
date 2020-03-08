9d4532b13e66266f86c91c5023fc227f13eab987
PeterFiorentino <peterfiorentino@pursuit.org>
2020-03-08 11:35:55 -0400

    adding a route to get the transactions by the users

--- backend/routes/tickers.js
+++ backend/routes/tickers.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const authHelpers = require('../auth/helpers')
-const usersQueries = require('../queries/users')
+const transactionQueries = require('../queries/transactions')
 
 
 router.get('/:user_id', authHelpers.loginRequired, async (req, res, next) => {
   try {
     const user_id = req.params.user_id;
-    const transactions = await usersQueries.getAllUsers()
+    const transactions = await transactionQueries.getTransactionsByUser()
     res.send({
-      payload: users,
-      msg: "Retrieved all users",
+      payload: transactions,
+      msg: `Retrieved all transactions from this user ${user_id}`,
       err: false
     })
   } catch (err) {
